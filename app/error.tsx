ec79f210fa9b28cfadb640d56cd8d2e901fecd8d
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-07-12 22:26:20 +0600

    basic setu[

---
+++ app/error.tsx
@@ -0,0 +1,16 @@
+// app/error.js
+'use client';
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
